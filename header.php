dfb3e8504889b4306782f021e8bc482dceab249b
eodaneze <ezealiokechukwu1999@gmail.com>
2023-08-02 09:24:29 -0700

    backend for the view blog section

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link rel="stylesheet" href="./assets/css/fontawesome.css">
     <link
         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
         rel="stylesheet">
--- viewBlog.php
+++ viewBlog.php
@@ -5,12 +5,45 @@
 
 ?>
 
+<style>
+    p span{
+        display: block;
+    }
+</style>
 <main class="main" id="main">
       <div class="pagetitle">
         
           <h1>View Blog | StandBlog</h1>
         </div>
      <div class="row">
+        <?php
+            $sql = "SELECT * FROM posts";
+            $result = mysqli_query($conn, $sql);
+            
+            while ($row = mysqli_fetch_assoc($result)) {
+                 $title = $row['title'];
+                 $pic = $row['blog_image'];
+                 $id = $row['post_id'];
+
+                 ?>
+                    <div class="col-lg-3">
+                             <img style="height: 200px;" class="img-fluid" src="./includes/blogImg/<?=$pic?>" alt="">
+                         <div class="text-center">
+                            <a href="./singleBlog.php"><h5><?=$title?></h5></a>
+                            <p>
+                                <span>Category: Artificial Intelligence</span>
+                                <span>Tag: Educational</span>
+                             </p>
+                            <a href=""><i class="fa fa-edit"></i></a>
+                            <a href="./includes/delete.php?id=<?=$id?>"><i class="fa fa-trash"></i></a>
+                        </div>
+                    </div>
+
+                <?php
+            }
+
+         ?>
+        
          
      </div>
 </main>
